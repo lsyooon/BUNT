b79f2a9b2aef6e427e336a559d940aa25df00b9a
lshyunee <a94460093@gmail.com>
2024-05-15 20:30:15 +0900

    Feat: CommunityController

---
+++ backend_project/src/main/java/com/baseball/bunt/model/service/CommunityServiceImpl.java
@@ -0,0 +1,42 @@
+package com.baseball.bunt.model.service;
+
+import com.baseball.bunt.model.dao.CommunityDao;
+import com.baseball.bunt.model.dto.community.CommunityBoard;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CommunityServiceImpl implements CommunityService{
+
+    private final CommunityDao dao;
+
+    @Override
+    public List<CommunityBoard> readBoardList() {   // 게시판 리스트
+        return dao.boardList();
+    }
+
+    @Override
+    public CommunityBoard readBoardDetail(final int communityBoardId) {   // 게시글 상세 보기
+        return dao.getDetail(communityBoardId);
+    }
+
+    @Override
+    public int createBoard(final CommunityBoard board) {  // 게시글 생성
+        return dao.insertBoard(board);
+    }
+
+    @Override
+    public int removeBoardByBoardId(final int communityBoardId) { // 게시글 삭제
+        return dao.deleteBoard(communityBoardId);
+    }
+
+    @Override
+    @Transactional
+    public int modifyBoardByBoardId(final CommunityBoard board) { // 게시글 수정
+        return dao.updateBoard(board);
+    }
+}
