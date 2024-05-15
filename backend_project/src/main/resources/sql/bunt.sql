4ba47c65c0da2586a868ed8f370260ce9c92e445
prao <pch5521@gmail.com>
2024-05-15 23:08:06 +0900

    Init: "Shop 기능 폐지"
    - 상점 기능 폐지
    - 그에 따른 코드 삭제
    - SQL Script 업데이트

--- backend_project/src/main/resources/sql/bunt.sql
+++ backend_project/src/main/resources/sql/bunt.sql
@@ -18,118 +18,6 @@ CREATE TABLE `USERS`
     PRIMARY KEY (`user_id`)
 );
 
-DROP TABLE IF EXISTS `ORDERS`;
-CREATE TABLE `ORDERS`
-(
-    `order_id`    INT         NOT NULL AUTO_INCREMENT,
-    `user_id`     INT         NOT NULL,
-    `delivery_id` INT         NOT NULL,
-    `order_date`   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `status`      VARCHAR(10) NOT NULL,
-    `total_price` INT         NOT NULL,
-    PRIMARY KEY (`order_id`)
-);
-
-DROP TABLE IF EXISTS `DELIVERY`;
-CREATE TABLE `DELIVERY`
-(
-    `delivery_id`      INT          NOT NULL AUTO_INCREMENT,
-    `status`           VARCHAR(10)  NOT NULL,
-    `address`          VARCHAR(100)  NOT NULL,
-    `delivery_request` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`delivery_id`)
-);
-
-DROP TABLE IF EXISTS `ORDER_ITEM`;
-CREATE TABLE `ORDER_ITEM`
-(
-    `order_item_id` INT NOT NULL AUTO_INCREMENT,
-    `order_id`      INT NOT NULL,
-    `item_id`       INT NOT NULL,
-    `count`      INT NOT NULL,
-    `order_price`         INT NOT NULL,
-    PRIMARY KEY (`order_item_id`)
-);
-
-DROP TABLE IF EXISTS `ITEM`;
-CREATE TABLE `ITEM`
-(
-    `item_id`        INT          NOT NULL AUTO_INCREMENT,
-    `team_id`        INT          NOT NULL,
-    `name`           VARCHAR(20)  NOT NULL,
-    `price`          INT          NOT NULL,
-    `stock_quantity` INT          NOT NULL,
-    `image_path`     VARCHAR(100) NOT NULL,
-    `category`       VARCHAR(10)  NOT NULL,
-    `item_detail`    TEXT         NOT NULL,
-    `reg_date`       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `mod_date`       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`item_id`)
-);
-
-DROP TABLE IF EXISTS `REVIEW`;
-CREATE TABLE `REVIEW`
-(
-    `review_id`  INT          NOT NULL AUTO_INCREMENT,
-    `user_id`    INT          NOT NULL,
-    `item_id`    INT          NOT NULL,
-    `title`      VARCHAR(100) NOT NULL,
-    `content`    TEXT         NOT NULL,
-    `image_path` VARCHAR(100) NOT NULL,
-    `reg_date`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `mod_date`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`review_id`)
-);
-
-DROP TABLE IF EXISTS `REVIEW_COMMENT`;
-CREATE TABLE `REVIEW_COMMENT`
-(
-    `review_comment_id` INT       NOT NULL AUTO_INCREMENT,
-    `user_id`           INT       NOT NULL,
-    `board_id`          INT       NOT NULL,
-    `content`           TEXT      NOT NULL,
-    `reg_date`          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `mod_date`          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`review_comment_id`)
-);
-
-DROP TABLE IF EXISTS `INQUIRY`;
-CREATE TABLE `INQUIRY`
-(
-    `inquiry_id` INT          NOT NULL AUTO_INCREMENT,
-    `user_id`    INT          NOT NULL,
-    `item_id`    INT          NOT NULL,
-    `title`      VARCHAR(100) NOT NULL,
-    `content`    TEXT         NOT NULL,
-    `reg_date`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `mod_date`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`inquiry_id`)
-);
-
-DROP TABLE IF EXISTS `INQUIRY_COMMENT`;
-CREATE TABLE `INQUIRY_COMMENT`
-(
-    `inquiry_comment_id` INT       NOT NULL AUTO_INCREMENT,
-    `user_id`            INT       NOT NULL,
-    `board_id`           INT       NOT NULL,
-    `content`            TEXT      NOT NULL,
-    `reg_date`           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `mod_date`           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`inquiry_comment_id`)
-);
-
-DROP TABLE IF EXISTS `CART`;
-CREATE TABLE `CART`
-(
-    `cart_id`      INT        NOT NULL AUTO_INCREMENT,
-    `user_id`      INT        NOT NULL,
-    `item_id`      INT        NOT NULL,
-    `quantity`     INT        NOT NULL,
-    `order_status` VARCHAR(5) NOT NULL,
-    `reg_date`     TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`cart_id`)
-);
-
 DROP TABLE IF EXISTS `TEAM`;
 CREATE TABLE `TEAM`
 (
@@ -149,16 +37,6 @@ CREATE TABLE `PLAYERS`
     `back_number` INT          NOT NULL,
     `song_path`   VARCHAR(100) NOT NULL,
     PRIMARY KEY (`player_id`)
-);
-
-DROP TABLE IF EXISTS `ZZIM`;
-CREATE TABLE `ZZIM`
-(
-    `zzim_id` INT         NOT NULL AUTO_INCREMENT,
-    `user_id` INT         NOT NULL,
-    `item_id` INT         NOT NULL,
-    `status`  VARCHAR(10) NOT NULL,
-    PRIMARY KEY (`zzim_id`)
 );
 
 DROP TABLE IF EXISTS `COMMUNITY`;
@@ -196,42 +74,8 @@ CREATE TABLE `COMMUNITY_BOARD_COMMENT`
     PRIMARY KEY (`community_board_comment_id`)
 );
 
-ALTER TABLE `ORDERS`
-    ADD CONSTRAINT `FK_USERS_TO_ORDERS_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);
-ALTER TABLE `ORDERS`
-    ADD CONSTRAINT `FK_DELIVERY_TO_ORDERS_1` FOREIGN KEY (`delivery_id`) REFERENCES `DELIVERY` (`delivery_id`);
-ALTER TABLE `ORDER_ITEM`
-    ADD CONSTRAINT `FK_ORDERS_TO_ORDER_ITEM_1` FOREIGN KEY (`order_id`) REFERENCES `ORDERS` (`order_id`);
-ALTER TABLE `ORDER_ITEM`
-    ADD CONSTRAINT `FK_ITEM_TO_ORDER_ITEM_1` FOREIGN KEY (`item_id`) REFERENCES `ITEM` (`item_id`);
-ALTER TABLE `ITEM`
-    ADD CONSTRAINT `FK_TEAM_TO_ITEM_1` FOREIGN KEY (`team_id`) REFERENCES `TEAM` (`team_id`);
-ALTER TABLE `REVIEW`
-    ADD CONSTRAINT `FK_USERS_TO_REVIEW_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);
-ALTER TABLE `REVIEW`
-    ADD CONSTRAINT `FK_ITEM_TO_REVIEW_1` FOREIGN KEY (`item_id`) REFERENCES `ITEM` (`item_id`);
-ALTER TABLE `REVIEW_COMMENT`
-    ADD CONSTRAINT `FK_USERS_TO_REVIEW_COMMENT_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);
-ALTER TABLE `REVIEW_COMMENT`
-    ADD CONSTRAINT `FK_REVIEW_TO_REVIEW_COMMENT_1` FOREIGN KEY (`board_id`) REFERENCES `REVIEW` (`review_id`);
-ALTER TABLE `INQUIRY`
-    ADD CONSTRAINT `FK_USERS_TO_INQUIRY_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);
-ALTER TABLE `INQUIRY`
-    ADD CONSTRAINT `FK_ITEM_TO_INQUIRY_1` FOREIGN KEY (`item_id`) REFERENCES `ITEM` (`item_id`);
-ALTER TABLE `INQUIRY_COMMENT`
-    ADD CONSTRAINT `FK_USERS_TO_INQUIRY_COMMENT_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);
-ALTER TABLE `INQUIRY_COMMENT`
-    ADD CONSTRAINT `FK_INQUIRY_TO_INQUIRY_COMMENT_1` FOREIGN KEY (`board_id`) REFERENCES `INQUIRY` (`inquiry_id`);
-ALTER TABLE `CART`
-    ADD CONSTRAINT `FK_USERS_TO_CART_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);
-ALTER TABLE `CART`
-    ADD CONSTRAINT `FK_ITEM_TO_CART_1` FOREIGN KEY (`item_id`) REFERENCES `ITEM` (`item_id`);
 ALTER TABLE `PLAYERS`
     ADD CONSTRAINT `FK_TEAM_TO_PLAYERS_1` FOREIGN KEY (`team_id`) REFERENCES `TEAM` (`team_id`);
-ALTER TABLE `ZZIM`
-    ADD CONSTRAINT `FK_USERS_TO_ZZIM_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);
-ALTER TABLE `ZZIM`
-    ADD CONSTRAINT `FK_ITEM_TO_ZZIM_1` FOREIGN KEY (`item_id`) REFERENCES `ITEM` (`item_id`);
 ALTER TABLE `COMMUNITY`
     ADD CONSTRAINT `FK_TEAM_TO_COMMUNITY_1` FOREIGN KEY (`team_id`) REFERENCES `TEAM` (`team_id`);
 ALTER TABLE `COMMUNITY_BOARD`
